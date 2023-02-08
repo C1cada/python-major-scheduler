219e3189260377d69924b8c98e11e61507c735c7
C1cada <Gabrielhwgo1@gmail.com>
2023-02-08 18:57:00 -0500

    exclusives

--- Scraper.py
+++ Scraper.py
@@ -4,13 +4,6 @@ from Program import Program
 from GenEd import GenEd
 import requests
 
-# url = "https://www.rit.edu/programs-api/?type=c&q=SWEN&college=&degree=&text="
-# xml_data = requests.get(url).content
-# soup = BeautifulSoup(xml_data, "xml")
-# # print(soup.contents[0].contents[3])
-# for i in soup.children:
-#     print(i)
-
 def get_class_data(code: str) -> Class:
     print(code)
     url = f"https://www.rit.edu/programs-api/?type=c&q={code}&college=&degree=&text="
@@ -44,36 +37,30 @@ def get_program_data(code: str) -> Program:
                 years = int(tr['data-heading'])
                 
     courses = []
+    exclusive_list = []
     for year in range(years):
         classes = program_data.program.curriculum_processed.table.tbody.find_all("tr", class_=f"hidden-row rows-{year+1}")
-        exclusive_list = []
         for course in classes:
             tds = course.find_all("td")
             
             if len(tds[0].text) < 2:
-                # c = GenEd(tds[1].text, int(tds[-1].text))
-                # print(c)
-                # print(course)
                 courses.append(GenEd(tds[1].text, int(tds[-1].text)))
+                if exclusive_list:
+                    courses.append(exclusive_list)
+                    exclusive_list = []
             elif tds[0].text == "Choose one of the following:":
-                exclusive_list = []
+                exclusive_list = [1]
             elif tds[0].text == "Choose two of the following:":
-                exclusive_list = []
+                exclusive_list = [2]
             elif ord(tds[0].text[0]) == 160:
-                courses.append(get_class_data(tds[0].text[3:]))
+                exclusive_list.append(get_class_data(tds[0].text[3:]))
             else:
+                if exclusive_list:
+                    courses.append(exclusive_list)
+                    exclusive_list = []
                 courses.append(get_class_data(tds[0].text))
             
     return Program(courses, code, program_data.program.title.text)
-            
-        
-            
-    # title = class_data.title.text
-    # desc = class_data.description.text
-    # credits = get_credits(class_data.credits.text)
-    # offered = class_data.typically_offered.text.split(", ")
-    # requisites = class_data.requisites.text
-    # classes.append(Class(title, code, desc, credits, offered, requisites))
     
 # print(str(get_class_data(["csec-123"])[0]))
 print(repr(get_program_data("PSYC-BS")))
